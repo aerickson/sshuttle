850180a3af42d13078cb207f62e10a218f17c48e
Andrew Erickson <aje@ticketfly.com>
2013-12-21 03:45:15 -0800

    formatting

--- ui-macos/askpass.py
+++ ui-macos/askpass.py
@@ -22,9 +22,9 @@ def askpass(prompt):
     rv = p.wait()
     if rv:
         return None
-      # 10.8 code
-#    g = re.match("text returned:(.*), button returned:.*", out)
-# 10.9 code
+    # 10.8 code
+    # g = re.match("text returned:(.*), button returned:.*", out)
+    # 10.9 code
     g = re.match("button returned:.*, text returned:(.*)", out)
     if not g:
         return None
